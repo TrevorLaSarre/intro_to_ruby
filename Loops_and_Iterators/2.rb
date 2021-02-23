6dc0502678e2fa378faea826629c7cd93ed5f1d8
Trevor LaSarre <trevorlasarre2@gmail.com>
2021-02-23 01:11:20 +0000

    Added Exercises for Chapter 5

---
+++ Loops_and_Iterators/2.rb
@@ -0,0 +1,6 @@
+answer = " "
+
+while answer != "STOP"
+  puts "Type a word. I'll only stop if you type STOP"
+  answer = gets.chomp
+end
---
+++ Loops_and_Iterators/3.rb
@@ -0,0 +1,11 @@
+def countdown(x)
+puts x
+  x -= 1
+  if x >= 0
+    countdown(x)
+  end
+end
+
+countdown(10)
+countdown(20)
+countdown(-3)
